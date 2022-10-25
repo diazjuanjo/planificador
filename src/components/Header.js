1c06c400b5174fb1d6a37650a9559e18eaa08dd3
Juanjo Diaz <diazjuanjose@icloud.com>
2022-10-25 18:30:32 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
+const Header = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.texto}>Planificador de Gastos</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  texto: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#FFF',
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    paddingTop: 20,
+  },
+});
+
+export default Header;
---
+++ src/components/NuevoPresupuesto.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {View, Text, TextInput, Pressable, StyleSheet} from 'react-native';
+
+const NuevoPresupuesto = () => {
+  return (
+    <View style={styles.contenedor}>
+      <Text>Definir Presupuesto</Text>
+      <TextInput />
+      <Pressable>
+        <Text>Agregar Presupuesto</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenedor: {
+    backgroundColor: '#FFF',
+    marginHorizontal: 10,
+    borderRadius: 10,
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+    transform: [{translateY: 50}],
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+  },
+});
+
+export default NuevoPresupuesto;
