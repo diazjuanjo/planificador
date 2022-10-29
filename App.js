c87dbccedffb2c76c543d9ba0a683d7dc2766276
Juanjo Diaz <diazjuanjose@icloud.com>
2022-10-29 15:52:30 -0300

    hasta clase 119

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import {View, ScrollView, StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, ScrollView, StyleSheet, Text, Alert} from 'react-native';
+import ControlPresupuesto from './src/components/ControlPresupuesto';
 import Header from './src/components/Header';
 import NuevoPresupuesto from './src/components/NuevoPresupuesto';
 
 const App = () => {
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
-  const handleNuevoPresupuesto = (presupuesto) => {
-    if(Number(presupuesto) > 0) {
-      console.log('presupuesto valido')
-  }else{
-    console.log('presupuesto inválido')
-  }}
+  const handleNuevoPresupuesto = presupuesto => {
+    if (Number(presupuesto) > 0) {
+      setIsValidPresupuesto(true);
+    } else {
+      Alert.alert('Error', 'El Presupuesto no puede ser 0 o menor', 'Ok');
+    }
+  };
 
   return (
     <View style={styles.contenedor}>
       <View style={styles.header}>
         <Header />
-        <NuevoPresupuesto 
-          handleNuevoPresupuesto={handleNuevoPresupuesto}
-        />
+        {isValidPresupuesto ? (
+          <ControlPresupuesto />
+        ) : (
+          <NuevoPresupuesto handleNuevoPresupuesto={handleNuevoPresupuesto} />
+        )}
       </View>
     </View>
   );
---
+++ src/components/ControlPresupuesto.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const ControlPresupuesto = () => {
+  return <Text>ControlPresupuesto</Text>;
+};
+
+export default ControlPresupuesto;
